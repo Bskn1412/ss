383aebec19b5579af1436f63d875401410005aa7
Bskn1412 <148748232+Bskn1412@users.noreply.github.com>
2024-04-19 22:19:23 +0530

    Update send_email.py

--- email/send_email.py
+++ email/send_email.py
@@ -1,79 +1,18 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from mail_notes import send_notes_email
-app = Flask(__name__)
 
+app = Flask(_name_)
+CORS(app)  
 
-@app.route('/submit/<email>/<notes>',methods=['POST','GET'])
-def submit(email,notes):
-        if request.method == "POST":
-            result = send_notes_email(email,notes)
-            if result == 'done':
-              return jsonify({"key":"sent"})
-            else:
-              return jsonify({"key":"not sent"})
+@app.route('/submit/<email>/<notes>', methods=['POST', 'GET'])
+def submit(email, notes):
+    if request.method == "POST" or request.method == 'GET':
+        result = send_notes_email(email, notes)
+        if result == 'done':
+            return jsonify({"key": "sent"})
         else:
-           print("only post not get")
-         
+            return jsonify({"key": "not sent"})
 
-if __name__ == "__main__":
-      app.run(debug=True)   
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-    
-    #@app.route('/check',methods=['POST','GET'])
-#def check():
- #   test = ''.join(list(request.form["n1"]+request.form["n2"]+request.form["n3"]+request.form["n4"]+request.form["n5"]+request.form["n6"])) 
- #   otp = request.form['otp']
- #   user = request.form['user']
- #   if test == otp:
- #       return render_template('home.html',rel=user)
-  #  return render_template("2_step.html",val="re-enter")
-#if __name__ == "__main__":
+if _name_ == "_main_":
+    app.run(debug=True)
